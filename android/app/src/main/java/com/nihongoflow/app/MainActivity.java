797b5aae3fd2868fe3030c08fc88b509f47c4c97
rohanny <yrohan740@gmail.com>
2026-01-02 22:06:03 +0530

    feat: converted app for android

---
+++ android/app/src/main/java/com/nihongoflow/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.nihongoflow.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,22 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.nihongoflow.app',
   appName: 'Nihongo',
-  webDir: 'dist'
+  webDir: 'dist',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      launchFadeOutDuration: 300,
+      backgroundColor: "#000000",
+      androidSplashResourceName: "splash",
+      showSpinner: false,
+      androidScaleType: "CENTER_CROP",
+      splashFullScreen: true,
+      splashImmersive: true,
+      layoutName: "launch_screen",
+      useDialog: true,
+    },
+  },
 };
 
 export default config;
